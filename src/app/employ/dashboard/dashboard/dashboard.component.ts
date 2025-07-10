b697e6cef0ce4abe0d8ea1234c334c4e4dd93ea8
SreehariSaiKrishna <jshsknaidu27@gmail.com>
2025-07-11 02:01:12 +0530

    Implemented logout

--- src/app/employ/dashboard/dashboard/dashboard.component.ts
+++ src/app/employ/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  constructor(public authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/hr/dashboard/hr-dashboard/hr-dashboard.component.ts
+++ src/app/hr/dashboard/hr-dashboard/hr-dashboard.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hr-dashboard',
   templateUrl: './hr-dashboard.component.html',
   styleUrl: './hr-dashboard.component.scss',
 })
-export class HrDashboardComponent {}
+export class HrDashboardComponent {
+  constructor(public authService: AuthService, public router: Router) {}
+
+  logout() {
+    this.authService.logout();
+  }
+}
