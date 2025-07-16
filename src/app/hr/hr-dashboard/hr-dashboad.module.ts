d5828124a60386cf027892b3136c18bd74f71fff
SreehariSaiKrishna <jshsknaidu27@gmail.com>
2025-07-16 23:45:22 +0530

    added icons

--- src/app/hr/hr-dashboard/hr-dashboad.module.ts
+++ src/app/hr/hr-dashboard/hr-dashboad.module.ts
@@ -15,6 +15,7 @@ import { MatOption } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [HrDashboardComponent, RegisterComponent],
@@ -35,6 +36,7 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    SharedModule,
   ],
 })
 export class HrDashboadModule {}
