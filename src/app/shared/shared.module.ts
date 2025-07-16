d00848a92eeb4693b6b14417e99a88b63c2d1410
SreehariSaiKrishna <jshsknaidu27@gmail.com>
2025-07-17 00:23:27 +0530

    created shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
@@ -21,7 +20,6 @@ import { Form16Component } from './form16/form16.component';
   ],
   imports: [
     CommonModule,
-    SharedRoutingModule,
     FormsModule,
     ReactiveFormsModule,
   ],
