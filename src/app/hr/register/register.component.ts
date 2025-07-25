ced3a7972242ec91c31774487f90c68b4837e0b5
saikrishna <jshsknaidu27@gmail.com>
2025-07-25 17:58:20 +0530

    fixed the issue with employ registration

--- src/app/hr/register/register.component.ts
+++ src/app/hr/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
+import { UtilityService } from '../../service/utility.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,12 @@ import { AuthService } from '../../service/auth.service';
 export class RegisterComponent {
   employeeForm: FormGroup;
 
-  constructor(private fb: FormBuilder, public authSvc: AuthService) {
+  constructor(
+    private fb: FormBuilder,
+    public authSvc: AuthService,
+    public utilityService: UtilityService,
+    private dialogRef: MatDialogRef<RegisterComponent>
+  ) {
     this.employeeForm = this.fb.group({
       name: ['', Validators.required],
       employeeId: ['', Validators.required],
@@ -19,14 +26,14 @@ export class RegisterComponent {
       department: ['', Validators.required],
       dateOfJoining: [''],
       dateOfBirth: [''],
-      password: [''],
+      password: ['123456'],
     });
   }
 
-  generatePassword() {
-    const password = Math.random().toString(36).slice(-10); // simple random password
-    this.employeeForm.patchValue({ password });
-  }
+  // generatePassword() {
+  //   const password = Math.random().toString(36).slice(-10); // simple random password
+  //   this.employeeForm.patchValue({ password });
+  // }
 
   onSubmit() {
     if (this.employeeForm.valid) {
@@ -40,10 +47,11 @@ export class RegisterComponent {
         })
         .catch((error) => {
           console.error('Error during registration:', error);
-          console.log('Form Data:', this.employeeForm.value);
+          this.utilityService.openSnackBar('Error during registration');
         });
     } else {
       console.warn('Form is invalid');
+      this.utilityService.openSnackBar('Employee data is invalid');
     }
   }
 
@@ -52,12 +60,18 @@ export class RegisterComponent {
     if (employeeData) {
       this.authSvc.saveEmployeeData(employeeData).then(
         () => {
-          console.log('Employee data saved successfully');
+          this.utilityService.openSnackBar('Employee data saved successfully');
+          this.dialogRef.close();
         },
         (error: any) => {
           console.error('Error saving employee data:', error);
         }
       );
     }
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+    this.employeeForm.reset();
   }
 }
